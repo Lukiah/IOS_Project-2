8843ad3a82414562eac2a3baa3368dffaf9c576e
Lukiah <lukas.zedekcz@gmail.com>
2022-04-28 23:49:32 +0200

    arguments parser fixed, skeleton partially done

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
-proj2:
-	gcc proj2.c -o proj2
+proj2: proj2.c
+	gcc -std=gnu99 -Wall -Wextra -Werror -pedantic -pthread -lrt proj2.c -o proj2
--- proj2.c
+++ proj2.c
@@ -2,23 +2,32 @@
 #include<stdio.h>
 #include<stdbool.h>
 #include<time.h>
+#include<unistd.h>
 
 //prototype section
 int mainProc(int nH, int nO, int lineWaitT, int molCreateT);
+void trash(); //cleans up leftover processes TODO
 
-int main(int argc, char *argv[]){  // will be used mainly for parsing args ---> "$ ./proj2 NO NH TI TB"
+int main(int argc, char *argv[]){
+    int nH = 0,nO = 0, tI, tB;
+    FILE *fOut; // proj2.out pointer
+
     if (argc == 5)
     {
-        if ((*argv[1] > 0) && (*argv[2] > 0)) // nH and nO input args check
+        nO = atoi(argv[1]);
+        nH = atoi(argv[2]);
+        tI = atoi(argv[3]);
+        tB = atoi(argv[4]);
+
+        if ((nO > 0) && (nH > 0) && (tI > 0) && (tB > 0)) // nO and nH input args check
         {
-            if ((0 <= *argv[3] <= 1000) && (0 <= *argv[4] <= 1000))
-            {
-                //initiate main process
-                return 1;
+            if ((tI <= 1000) && (tB <= 1000)) // timer check 
+            {   
+                //param checks passed
             }
             else
             {
-                fprintf(stderr, "negative/zero value timer parameters were entered\n"); //errcode for n0 <= TI/TB <= 1000
+                fprintf(stdout, "timer parameters outside of allowed range\n"); //errcode for n0 <= TI/TB <= 1000
                 return 1;    
             }
         }
@@ -30,14 +39,34 @@ int main(int argc, char *argv[]){  // will be used mainly for parsing args ---> 
     } 
     else
     {
-        fprintf(stderr, "invalid amount of parameters entered\n"); //errcode for argc != 5
+        fprintf(stderr, "invalid amount of parameters\n"); //errcode for argc != 5
         return 1;
     }
 
-}
+    if ((fOut = fopen("proj2.out", "w")) == NULL){ // invalid pointer to proj2.out
+        fprintf(stderr, "An error has occured while opening proj2.out file\n");
+        return 1;
+    }
 
-int mainProc(int nH, int nO, int lineWaitT, int molCreateT) {
+    for (int i = 1; i <= nO; i++){ // Oxygen processes creator
+        pid_t id = fork();
+        if(id == 0){ //then successor/child process
+            //oxygen(i); //TODO
+            exit(0);
+        }
+    }
 
-    //creates a certain number of H processes and O processes and gets their 
+    for (int i = 1; i <= nH; i++){ // Hydrogen processes creator
+        pid_t id = fork();
+        if(id == 0){ //then successor/child process
+            //hydrogen(i); //TODO
+            exit(0);
+        }
+    }
+
+    //while(wait(NULL) > 0);
+    //trash();
+
+
 
 }
