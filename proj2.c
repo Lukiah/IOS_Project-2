1c182c2b8bc51fedfc95b67d10606f76a061b004
Lukiah <lukas.zedekcz@gmail.com>
2022-04-30 20:31:44 +0200

    skeleton done, flushPrint fc added, ...

--- proj2.c
+++ proj2.c
@@ -1,17 +1,59 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<stdbool.h>
-#include<time.h>
-#include<unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <time.h>
+#include <unistd.h>
+#include <semaphore.h> 
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <sys/mman.h>
 
-//prototype section
-int mainProc(int nH, int nO, int lineWaitT, int molCreateT);
+
+sem_t *mutex;
+sem_t *printSem;
+FILE *fOut; // proj2.out pointer
+int lineNum = 1;
+
+void semaphoreInit(){ //initializes a singular semaphore 
+    mutex = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANON | MAP_SHARED, -1, 0);
+    sem_init(mutex,1,1);
+}
+
+void semaphoreDstr(void *shMem){ //destroy semaphore
+    sem_destroy(mutex);
+    munmap(shMem, sizeof(sem_t));
+}
+
+void flushPrint (const char * format, ...)
+{
+    sem_wait(printSem); // wait until noone else is writing into file
+    va_list args;
+    va_start (args, format);
+    fprintf(fOut, "%d: ",lineNum); //'lineNum:-'
+    vfprintf (fOut, format, args);
+    lineNum++;
+    fflush(fOut);
+    va_end (args);
+    sem_post(printSem); // free the mutex for other processes to write to file
+}
+
+void oFc(int oNum){
+    //TODO rest of oxygen business
+    flushPrint("O %d: ",oNum); // '-O-oNum:-' 
+}
+
+void hFc(int hNum){
+    //TODO rest of hydrogen business
+    flushPrint("H %d: ",hNum); // '-H-hNum:-'
+}
+
 void trash(); //cleans up leftover processes TODO
 
 int main(int argc, char *argv[]){
     int nH = 0,nO = 0, tI, tB;
-    FILE *fOut; // proj2.out pointer
-
+    
     if (argc == 5)
     {
         nO = atoi(argv[1]);
@@ -43,29 +85,34 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
-    if ((fOut = fopen("proj2.out", "w")) == NULL){ // invalid pointer to proj2.out
+    // opens(creates) file or prints on stderr if an error occurs
+    if ((fOut = fopen("proj2.out", "w")) == NULL){ 
         fprintf(stderr, "An error has occured while opening proj2.out file\n");
         return 1;
-    }
-
-    for (int i = 1; i <= nO; i++){ // Oxygen processes creator
-        pid_t id = fork();
-        if(id == 0){ //then successor/child process
-            //oxygen(i); //TODO
-            exit(0);
-        }
     }
 
     for (int i = 1; i <= nH; i++){ // Hydrogen processes creator
         pid_t id = fork();
         if(id == 0){ //then successor/child process
-            //hydrogen(i); //TODO
+            sem_wait(mutex);
+            hFc(i);
+            sem_post(mutex);
+            exit(0);
+        }
+    }
+
+    for (int i = 1; i <= nO; i++){ // Oxygen processes creator
+        pid_t id = fork();
+        if(id == 0){ //then successor/child process
+            sem_wait(mutex);
+            oFc(i);
+            sem_post(mutex);
             exit(0);
         }
     }
 
     //while(wait(NULL) > 0);
-    //trash();
+    //trash(); //after all child processes are done
 
 
 
