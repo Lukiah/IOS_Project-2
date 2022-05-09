df12cf65abecae8bd50e66bc973342c688f486ac
Lukiah <lukas.zedekcz@gmail.com>
2022-05-09 18:52:19 +0200

    debux attempt backup

--- proj2.c
+++ proj2.c
@@ -11,17 +11,14 @@
 #include <string.h>
 #include <sys/mman.h>
 
-// $ ./proj2 NO NH TI TB
-
-
-sem_t *baseMutex;
-sem_t *printSem; // write to file semaphore
+sem_t *printSem;
 sem_t *qMutex;
 sem_t *oQueue; 
 sem_t *hQueue;
 sem_t *barrierMutex;
 sem_t *turnstile1;
 sem_t *turnstile2;
+sem_t *killMutex;
 
 
 FILE *fOut; // proj2.out pointer
@@ -29,6 +26,8 @@ int *lineNum;
 int *molNum;
 int *hydrogen, *oxygen;
 int *cnt;
+int *possibleMolecules;
+int *killRemaining;
 
 void initSequence(){
     //shared memory section
@@ -65,14 +64,23 @@ void initSequence(){
         fprintf(stderr, "An attempt to map memory has failed.");
         exit(1);
     }
+    (*cnt)=0;
+
+    possibleMolecules = mmap(NULL, sizeof(*possibleMolecules), PROT_READ | PROT_WRITE, MAP_ANON | MAP_SHARED, -1, 0); // lineNum is now a shared memory variable
+    if (possibleMolecules == MAP_FAILED){
+        fprintf(stderr, "An attempt to map memory has failed.");
+        exit(1);
+    }
+
+    killRemaining = mmap(NULL, sizeof(*killRemaining), PROT_READ | PROT_WRITE, MAP_ANON | MAP_SHARED, -1, 0); // lineNum is now a shared memory variable
+    if (killRemaining == MAP_FAILED){
+        fprintf(stderr, "An attempt to map memory has failed.");
+        exit(1);
+    }
+    (*killRemaining)=0;
 
 
     //semaphore section
-    baseMutex = sem_open("/xzedek03_baseMutex", O_CREAT | O_EXCL, 0666, 1);
-    if (baseMutex == SEM_FAILED){ // failed to initialize
-        fprintf(stderr, "An attempt to initialize a semaphore has failed.\n");
-        exit(1);
-    }
     printSem = sem_open("/xzedek03_printSem", O_CREAT | O_EXCL, 0666, 1);
     if (printSem == SEM_FAILED){ // failed to initialize
         fprintf(stderr, "An attempt to initialize a semaphore has failed.\n");
@@ -108,14 +116,15 @@ void initSequence(){
         fprintf(stderr, "An attempt to initialize a semaphore has failed.\n");
         exit(1);
     }
-
-
+    killMutex = sem_open("/xzedek03_killMutex", O_CREAT | O_EXCL, 0666, 1);
+    if (killMutex == SEM_FAILED){ // failed to initialize
+        fprintf(stderr, "An attempt to initialize a semaphore has failed.\n");
+        exit(1);
+    }
 }
 
-void trash(){ 
+void trash(){ //cleans up semaphores and shared memory
     //semaphore destroying section
-    sem_close(baseMutex);
-    sem_unlink("/xzedek03_baseMutex");
     sem_close(printSem);
     sem_unlink("/xzedek03_printSem");
     sem_close(oQueue);
@@ -130,9 +139,8 @@ void trash(){
     sem_unlink("/xzedek03_turnstile1");
     sem_close(turnstile2);
     sem_unlink("/xzedek03_turnstile2");
-    //sem_close(another_semaphore);
-    //sem_unlink("/xzedek03_another_semaphore");
-
+    sem_close(killMutex);
+    sem_unlink("/xzedek03_killMutex");
 
     //memory unmapping section
     munmap(lineNum, sizeof(*lineNum));
@@ -140,9 +148,11 @@ void trash(){
     munmap(hydrogen, sizeof(*hydrogen));
     munmap(oxygen, sizeof(*oxygen));
     munmap(cnt, sizeof(*cnt));
+    munmap(possibleMolecules, sizeof(*possibleMolecules));
+    munmap(killRemaining, sizeof(*killRemaining));
 }
 
-void flushPrint (char * text, ...) //TODO přepsat stdout na fOut v teto funkci a všude jinde v kódu
+void flushPrint (char * text, ...) // TODO change stdout to *fOut
 {
     sem_wait(printSem); // wait until noone else is writing into file
     va_list args;
@@ -152,7 +162,7 @@ void flushPrint (char * text, ...) //TODO přepsat stdout na fOut v teto funkci 
     vfprintf(stdout, text, args);
     fflush(stdout);
     va_end (args);
-    sem_post(printSem); // free the mutex for other processes to write to file
+    sem_post(printSem);
 }
 
 void oFc(int oNum, int TI, int TB){   
@@ -172,10 +182,13 @@ void oFc(int oNum, int TI, int TB){
     } else {
         sem_post(qMutex);
     } 
-    sem_wait(oQueue); //post this x times for each excess O
+    sem_wait(oQueue);
     //bond()
-    //check if enough hydro TODO -> kill leftover processes
-    //if excess == TRUE then print(not enough) and return(0);
+
+    if ((*killRemaining)){ //cleanup
+        flushPrint("O: %d: not enough H\n", oNum);
+        return;
+    }
 
     flushPrint("O: %d: creating molecule %d\n", oNum, (*molNum));
 
@@ -206,10 +219,20 @@ void oFc(int oNum, int TI, int TB){
     sem_wait(turnstile2);
     sem_post(turnstile2);
     //barrier.wait() END
-
     (*molNum)++;
     sem_post(qMutex);    
 
+    sem_wait(killMutex);
+    if ((*possibleMolecules)<(*molNum)){ //last molecule
+        (*killRemaining)=1;
+        for(int j = 0; j < (*oxygen); j++){
+            sem_post(oQueue);
+        }
+        for(int k = 0; k < (*hydrogen); k++){
+            sem_post(hQueue);
+        }
+    } 
+    sem_post(killMutex);
 
 }
 
@@ -231,7 +254,12 @@ void hFc(int hNum, int TI){
         sem_post(qMutex); 
     }
     sem_wait(hQueue); //post this x times for each excess H
-    //check if enough oxy TODO
+
+    if ((*killRemaining)){ //cleanup
+        flushPrint("H: %d: not enough O or H\n", hNum);
+        return;
+    }
+
     flushPrint("H: %d: creating molecule %d\n", hNum, (*molNum));
 
     //barrier.wait() START
@@ -261,12 +289,8 @@ void hFc(int hNum, int TI){
     //barrier.wait() END
 }
 
-void trash(); //cleans up leftover processes TODO
-
 int main(int argc, char *argv[]){
     int nH = 0, nO = 0, tI, tB;
-    
-    //semaphoresDstr(lineNum); //semaphores failing to initialize <- hotfix
 
     if (argc == 5)
     {
@@ -279,8 +303,16 @@ int main(int argc, char *argv[]){
         {
             if ((tI <= 1000) && (tB <= 1000)) // timer check 
             {   
+                trash();
+                trash();
+                trash();
                 initSequence();
+                (*possibleMolecules)=(((nH / 2) < (nO)) ? (nH / 2) : (nO));
 
+                if ((fOut = fopen("proj2.out", "w")) == NULL){ 
+                    fprintf(stderr, "An error has occured while opening proj2.out file\n");
+                    exit(1);
+                }
                 for (int i = 1; i <= nH; i++){ // Hydrogen processes creator
                     pid_t id = fork();
                     if(id == 0){ //== successor/child process
@@ -296,33 +328,28 @@ int main(int argc, char *argv[]){
                         exit(0);
                     }
                 }
-
+                
                 while(wait(NULL) > 0); //main process waits until all its child processes are done
-                printf("Parent: 'All the bastards are done. Finally, I can rest.'\n");
                 trash();
+                fclose(fOut);
+                flushPrint("program run successful\n");
             }
             else
             {
-                fprintf(stdout, "timer parameters outside of allowed range\n"); //errcode for n0 <= TI/TB <= 1000
+                fprintf(stderr, "timer parameters outside of allowed range\n"); //for n0 <= TI/TB <= 1000
                 exit(1);    
             }
         }
         else
         {
-            fprintf(stderr, "negative arguments or arguments equal to zero entered\n"); //errcode for nO/nH <= 0
+            fprintf(stderr, "negative arguments or arguments equal to zero entered\n"); //for nO/nH <= 0
             exit(1); 
         }
     } 
     else
     {
-        fprintf(stderr, "invalid amount of parameters\n"); //errcode for argc != 5
+        fprintf(stderr, "invalid amount of parameters\n"); //for argc != 5
         exit(1);
     }
 
-    // opens(creates) file or prints on stderr if an error occurs MOVE TO CORRECT PLACE TODO
-    if ((fOut = fopen("proj2.out", "w")) == NULL){ 
-        fprintf(stderr, "An error has occured while opening proj2.out file\n");
-        exit(1);
-    }
-    fclose(fOut);
 }
